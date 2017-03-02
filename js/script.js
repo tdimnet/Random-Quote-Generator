dc4f59dbbbd78a39de865ede15dd0984f840db1c
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-03-02 09:07:32 +0100

    Start adding the variables needed in order to work on the project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
+
+//
+  // Initial code
+//
+/*
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+*/
+
+
+// Selecting the variables within the DOM
+var quoteBox      = document.getElementById('quote-box');
+var quoteItSelf   = quoteBox.getElementsByClassName('quote');
+var author        = quoteBox.getElementsByClassName('author');
+var citation      = quoteBox.getElementsByClassName('citation');
+var year          = quoteBox.getElementsByClassName('year');
+var loadQuoteBtn  = document.getElementById('loadQuote');
+
+
+// Adding the event listeners
+loadQuoteBtn.onclick = function() {
+  console.log(quoteBox);
+  console.log(quoteItSelf);
+  console.log(author);
+  console.log(citation);
+  console.log(year);
+  console.log(loadQuoteBtn);
+};
